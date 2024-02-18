fd6d63666e9378c70982048a01f6f2b05c7ae8e9
unknown <tomas_toledo98@hotmail.com>
2024-02-18 11:37:35 -0300

    card optimizado

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,16 +3,11 @@ import Card from './Card';
 export default function Cards({characters}) {
    return <div>
    {
-      characters.map((character) => {
+      characters.map((personaje) => {
          return <Card
-            id={character.id}
-            name={character.name}
-            status={character.status}
-            species={character.species}
-            gender={character.gender}
-            origin={character.origin.name}
-            image={character.image}
+            key={personaje.id}
             onClose={() => window.alert('Emulamos que se cierra la card')}
+            {...personaje}
          />
       })
    }
