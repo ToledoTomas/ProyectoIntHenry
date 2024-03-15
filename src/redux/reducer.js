121ab45fae28c44082ee729319ceef1cac22d51e
unknown <tomas_toledo98@hotmail.com>
2024-03-14 23:24:30 -0300

    funcionalidades completadas

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,19 +1,47 @@
-import { ADD_FAV, REMOVE_FAV } from "./actions-types";
+import { ADD_FAV, FILTER, ORDER, REMOVE_FAV } from "./actions-types";
 
 const initialState = {
-    myFavorites:[]
+    myFavorites:[],
+    allCharacters:[]
 }
 
 function rootReducer (state = initialState, action) {
     switch (action.type){
 
         case ADD_FAV:
-            return{...state, myFavorites:[...state.myFavorites, action.payload]};
+            return{
+            ...state,
+            myFavorites:[...state.myFavorites, action.payload],
+            allCharacters:[...state.allCharacters, action.payload]};
 
         case REMOVE_FAV:
-            return {...state, myFavorites:state.myFavorites.filter(
-                (char) => char.id !== Number(action.payload)
+            return {
+            ...state, myFavorites:state.myFavorites.filter(
+            (char) => char.id !== Number(action.payload)
             )};
+
+        case FILTER:
+            const filteredCharacters = 
+            action.payload === "ALL" 
+            ? state.allCharacters
+            : state.allCharacters.filter((char) => {
+                return char.gender === action.payload
+            });
+
+            return{
+                ...state,
+                myFavorites:filteredCharacters,
+            }
+
+        case ORDER:
+            const orderFavs = state.myFavorites.sort((a,b)=>{
+                return action.payload === "A" ? a.id - b.id : b.id - a.id
+            })
+
+            return{
+                ...state,
+                myFavorites:orderFavs
+            }
 
         default: 
         return {...state}
--- src/views/Favorites/Favorites.jsx
+++ src/views/Favorites/Favorites.jsx
@@ -1,9 +1,41 @@
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import Cards from '../../components/Cards/Cards';
+import { filterCards, orderCards } from "../../redux/actions";
+import { useState } from "react";
 
 const Favorites = ({myFavorites}) => {
+
+    const [aux, setAux] = useState(false);
+
+    
+    const dispatch = useDispatch()
+
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value));
+        setAux(!aux)
+    }
+
+    const handleFilter = (event) => {
+        dispatch(filterCards(event.target.value))
+    }
+
     return (
         <>
+        <label htmlFor="Order">Filtrar por orden:</label>
+        <select onChange={handleOrder}>
+            <option value="A">Ascendant</option>
+            <option value="D">Descendant</option>
+        </select>
+
+        <label htmlFor="Filter">Filtrar por género:</label>
+        <select onChange={handleFilter}>
+            <option value="All">All</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Genderless">Genderless</option>
+            <option value="unkown">Unknown</option>
+        </select>
+
         <Cards characters={myFavorites}/>
         </>
     )
