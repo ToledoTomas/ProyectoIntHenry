366d0ee0236afdf63e5c804828b9504bab344fb9
unknown <tomas_toledo98@hotmail.com>
2024-05-01 18:44:42 -0300

    bdd creada

--- Back/src/index.js
+++ Back/src/index.js
@@ -1,10 +1,20 @@
+require('./DB_connection.js')
 const server = require('./app.js');
 const PORT = 3001;
+const {conn} = require('./DB_connection.js')
 
+try {
+    server.listen(PORT, async () => {
+        console.log("Server raised in port: " + PORT)
+        await conn.sync({force: false})
+    })
+} catch (error) {
+    console.log(error);
+}
 
-server.listen(PORT, () => {
-    console.log("Server raised in port: " + PORT)
-})
+// server.listen(PORT, () => {
+//     console.log("Server raised in port: " + PORT)
+// })
 
 // http.createServer((req, res) => {
 //     const { url } = req;
